785ccb6abe54ef689f50424bdc45e0238d76652d
Melanie Beck <melaniebeck81@gmail.com>
2020-06-24 11:53:23 -0700

    rename

---
+++ src/readers.py
@@ -0,0 +1,74 @@
+####################################
+#  Class and utils for the Reader  #
+####################################
+import os
+from transformers import pipeline
+
+cwd = os.getcwd()
+
+# TODO: hardcode model paths that we trained 
+MODEL_PATHS = {
+    'default_bert_base_uncased': 'bert-base-uncased',
+    'bert_base_uncased_squad1':
+        cwd+"/src/models/bert/bert-base-uncased-tuned-squad-1.0",
+    'bert_base_cased_squad2':
+        cwd+"/src/models/bert/bert-base-cased-tuned-squad-2.0/"
+}
+
+# What does the READER need?
+# -- model and tokenizer
+# -- ability to chunk text 
+# -- more sophisticated prediction method 
+
+# What classes should the READER interact with? 
+# -- EXAMPLE class: can have an optional "answers" for examples that have answers for validation
+# -- FEATURES class: stores the model-ingestible features of that CONTEXT
+# -- RESULTS class: handles the output logits? Might not be necessary
+# -- PIPELINE class: the thing that connects the reader to the retriever
+
+class Reader:
+    def __init__(
+        self, 
+        model_name="twmkn9/distilbert-base-uncased-squad2",
+        tokenizer_name="twmkn9/distilbert-base-uncased-squad2",
+        top_n_per_doc=3,
+        use_gpu=True,
+        handle_impossible_answer=True
+        ):
+        
+        self.use_gpu = use_gpu
+        self.model = pipeline('question-answering', model=model_name, tokenizer=tokenizer_name, device=int(use_gpu)-1)
+        self.kwargs = {'topk':top_n_per_doc, 'handle_impossible_answer':handle_impossible_answer}
+
+    def predict(self, question, documents):
+        # instead of having to deal with all the nitty gritty details of converting examples into features 
+        # and creating sensible predictions, we can instead focus on predicting answers over a collection of docs
+        # as this is what we'll be getting from the retriever
+
+        all_predictions = []
+        for doc in documents:            
+            inputs = {"question": question, "context": doc['text']}
+            predictions = self.model(inputs, **self.kwargs)
+
+            # store these for each document for ranking at the end
+            for pred in predictions:
+                answer = {
+                    'probability': pred['score'],
+                    'answer_text': pred['answer'],
+                    'start_index': pred['start'],
+                    'end_index': pred['end'],
+                    'doc_id': doc['id'],
+                    'title': doc['title']
+                }
+                all_predictions.append(answer)
+        
+        # Once we have all possible answers from all documents, sort them and take the top_n
+        # This method does not take into account how highly ranked a document is (doc might have a score too)
+        best_predictions = sorted(all_predictions, key=lambda x: x["probability"], reverse=True)[: self.kwargs["topk"]]
+
+        results = {
+            'question': question,
+            'answers': best_predictions
+        }
+        return results
+
---
+++ src/retrievers.py
@@ -0,0 +1,159 @@
+#######################################
+#  Class and utils for the Retriever  #
+#######################################
+
+# What does the RETRIEVER need? 
+# a method for instantiation
+# pipeline to connect to a running instance
+# query processing
+# document fetching
+# document processing
+
+# What classes does the RETRIEVER need to interact with? 
+# EXAMPLE class: if this is an evaluation setting, the example class will have the document title 
+# PIPELINE class: the thing that connects the retriever to the reader
+import re
+import os
+import json
+import pickle as pkl
+import logging
+import time
+from tqdm import tqdm
+from elasticsearch import Elasticsearch
+
+def load_corpus(filename):
+    if os.path.exists(filename):
+        corpus = pkl.load(open(filename, 'rb'))
+        return corpus
+    print(r"{filename} does not exist!")
+    return
+
+class Retriever():
+
+    def __init__(
+        self, 
+        index_name='demo_index',
+        settings={
+            "mappings": {
+                "dynamic": "strict",        
+                "properties": {
+                    "document_title": {"type": "text", "analyzer": "standard_analyzer"},
+                    "document_text": {"type": "text", "analyzer": "standard_analyzer"}
+                    }
+                }
+            },
+        corpus_filename = None,
+        evidence_corpus = None,
+        ):
+        self.index_name = index_name
+        self.settings = settings
+        self.corpus_filename = corpus_filename
+        
+        if corpus_filename and not evidence_corpus:
+            evidence_corpus = load_corpus(corpus_filename)
+        
+        if not corpus_filename and not evidence_corpus:
+            try:
+                self.corpus_filename = 'evidence_corpus_mini.pkl'
+                evidence_corpus = load_corpus(self.corpus_filename)
+            except:
+                print("Default corpus was not found.")
+  
+        self.es = self.connect_es()
+        self.create_es_index()
+        self.load_es_index(evidence_corpus)
+
+    def connect_es(self, host='localhost', port=9200):
+        '''
+        Instantiate and return a Python ElasticSearch object
+
+        Args:
+            host (str)
+            port (int)
+        
+        Returns:
+            es (elasticsearch.client.Elasticsearch)
+
+        '''
+        
+        config = {'host':host, 'port':port}
+
+        try:
+            es = Elasticsearch([config])
+
+        except Exception as e:
+            logging.error('Couldnt connect to ES server', exc_info=e)
+
+        return es
+
+    def create_es_index(self):
+        '''
+        Create an ElasticSearch index
+
+        Args:
+            es_obj (elasticsearch.client.Elasticsearch)
+            settings (dict)
+            index_name (str)
+
+        '''
+
+        self.es.indices.create(index=self.index_name, body=self.settings, ignore=400)
+
+        logging.info('Index created successfully!')
+
+        return
+
+    def load_es_index(self, evidence_corpus):
+        '''
+        Loads records into an existing ElasticSearch index
+
+        Args:
+            es_obj (elasticsearch.client.Elasticsearch)
+            index_name (str)
+            evidence_corpus (list) - list of dicts containing data records
+
+        '''
+
+        for i, rec in enumerate(tqdm(evidence_corpus)):
+        
+            try:
+                index_status = self.es.index(index=self.index_name, id=i, body=rec)
+
+            except Exception as e:
+                logging.error(f'Error loading doc with index {i}', exc_info=e)
+        
+        time.sleep(10)
+        n_records = self.es.count(index=self.index_name)['count']
+        logging.info(f'Succesfully loaded {n_records} into {self.index_name}')
+
+        return
+
+    def run_question_query(self, question_text, n_results=5):
+        '''
+
+        '''
+        # construct query
+        """
+        query = {
+                'query': {
+                    'query_string': {
+                        'query': re.sub('[^A-Za-z0-9]+', ' ', question_text),
+                        'default_field': 'document_text_clean'
+                        }
+                    }
+                }
+        """
+        query = {
+                'query': {
+                    'match': {
+                        'document_text': question_text
+                        }
+                    }
+                }
+
+        # execute query
+        res = self.es.search(index=self.index_name, body=query, size=n_results)
+
+        #TODO run some checks / cleaning on the results - return something easier to parse
+
+        return res
