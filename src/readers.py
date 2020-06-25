aec158b3a87feed2af635ef8e50ba67b1f34b509
Melanie Beck <melaniebeck81@gmail.com>
2020-06-24 22:36:12 -0700

    new prediction method that aggregates docs before passing to transformer

--- src/readers.py
+++ src/readers.py
@@ -1,7 +1,6 @@
 ####################################
 #  Class and utils for the Reader  #
 ####################################
-# collapse-hide
 import os
 from transformers import pipeline
 
@@ -11,46 +10,27 @@ class Reader:
         model_name="twmkn9/distilbert-base-uncased-squad2",
         tokenizer_name="twmkn9/distilbert-base-uncased-squad2",
         use_gpu=True,
-        topk=3,
         handle_impossible_answer=True
         ):
         
         self.use_gpu = use_gpu
         self.model = pipeline('question-answering', model=model_name, tokenizer=tokenizer_name, device=int(use_gpu)-1)
-        self.kwargs = {'handle_impossible_answer':handle_impossible_answer, 'topk':topk}
+        self.kwargs = {'handle_impossible_answer':handle_impossible_answer}
 
     def predict(self, question, documents, topk=None):
-        """
-        Compute text prediction for a question given a collection of documents
-
-        Inputs:
-            question: str, question string
-            documents: list of document dicts, each with the following format:
-                    {
-                        'text': context string,
-                        'id': document identification,
-                        'title': name of document 
-                    }
-            topk (optional): int, if provided, overrides default topk
-        
-        Outputs:
-            results: dict with the following format:
-                {
-                    'question': str, question string,
-                    'answers': list of answer dicts, each including text answer, probability, 
-                                start and end positions, and document metadata
-                }
-        """
-        if topk:
-            self.kwargs['topk'] = topk
+        # instead of having to deal with all the nitty gritty details of converting examples into features 
+        # and creating sensible predictions, we can instead focus on predicting answers over a collection of docs
+        # as this is what we'll be getting from the retriever
+        self.kwargs['topk'] = topk
 
         all_predictions = []
         for doc in documents:            
             inputs = {"question": question, "context": doc['text']}
             predictions = self.model(inputs, **self.kwargs)
-            
+            print(predictions)
+
             # we want the best prediction from each document
-            if self.kwargs['topk'] == 1:
+            if topk == 1:
                 best = predictions
             else:
                 best = predictions[0]
@@ -84,6 +64,64 @@ class Reader:
         else:  
             # sort null answers for funsies
             best_predictions = sorted(all_predictions, key=lambda x: x["probability"], reverse=True)[: self.kwargs["topk"]]
+        
+        results = {
+            'question': question,
+            'answers': best_predictions
+        }
+        return results
+
+    def predict_combined(self, question, documents, topk=None):
+        """
+        Compute text prediction for a question given a collection of documents
+
+        Inputs:
+            question: str, question string
+            documents: list of document dicts, each with the following format:
+                    {
+                        'text': context string,
+                        'id': document identification,
+                        'title': name of document 
+                    }
+            topk (optional): int, if provided, overrides default topk
+        
+        Outputs:
+            results: dict with the following format:
+                {
+                    'question': str, question string,
+                    'answers': list of answer dicts, each including text answer, probability, 
+                                start and end positions, and document metadata
+                }
+        """
+        if topk:
+            self.kwargs['topk'] = topk
+
+        # combine all documents together into one long context
+        context = ''
+        for doc in documents:
+            context += doc['text'] + ' '
+
+        all_predictions = []
+        inputs = {"question": question, "context": context}
+        predictions = self.model(inputs, **self.kwargs)
+        
+        # we want the best prediction from each document
+        if self.kwargs['topk'] == 1:
+            predictions = [predictions]
+
+        for pred in predictions:
+            answer = {
+                    'probability': pred['score'],
+                    'answer_text': pred['answer'],
+                    'start_index': pred['start'],
+                    'end_index': pred['end'],
+                    #'doc_id': doc['id'],
+                    #'title': doc['title']
+                }
+            all_predictions.append(answer)
+
+        # sort and truncate predictions
+        best_predictions = sorted(all_predictions, key=lambda x: x["probability"], reverse=True)[: self.kwargs["topk"]]
         
         results = {
             'question': question,
